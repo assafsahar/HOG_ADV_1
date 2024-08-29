d6fdb18480be931505c09feff91876ed106d0698
assafsahar <assaf@codactive.com>
2024-08-29 16:20:47 +0300

    implemented a parallax mechanism (taken from easy parallax)

---
+++ Assets/_HOG/Scripts/UI/HOGParallaxMovementSpeedType.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace HOG.UI
+{
+[CreateAssetMenu(fileName = "MovementSpeedType", menuName = "Parallax/Movement Speed Type")]
+public class HOGParallaxMovementSpeedType : ScriptableObject
+{
+    public float speed = 1f;
+}
+   
+}
